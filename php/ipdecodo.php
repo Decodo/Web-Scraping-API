9b74839cc58a2fc40b71bd6f570b2b3412208e2b
Žilvinas <159907476+sakanamk@users.noreply.github.com>
2025-04-22 13:48:40 +0300

    Update and rename ipsmartproxy.php to ipdecodo.php

--- php/ipdecodo.php
+++ php/ipdecodo.php
@@ -3,8 +3,8 @@ require_once('vendor/autoload.php');
 
 $client = new \GuzzleHttp\Client();
 
-$response = $client->request('POST', 'https://scraper-api.smartproxy.com/v2/scrape', [
-  'body' => '{"target":"universal","parse":false,"url":"https://ip.smartproxy.com/"}',
+$response = $client->request('POST', 'https://scraper-api.decodo.com/v2/scrape', [
+  'body' => '{"target":"universal","parse":false,"url":"https://ip.decodo.com/"}',
   'headers' => [
     'Accept' => 'application/json',
     'Authorization' => 'Basic AUTH',
