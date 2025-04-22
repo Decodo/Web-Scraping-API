6685239a78afef06e67c1711ff209fecc30d2aab
Žilvinas <159907476+sakanamk@users.noreply.github.com>
2025-04-22 13:46:17 +0300

    Update facebookgroup.php

--- php/facebookgroup.php
+++ php/facebookgroup.php
@@ -3,7 +3,7 @@ require_once('vendor/autoload.php');
 
 $client = new \GuzzleHttp\Client();
 
-$response = $client->request('POST', 'https://scraper-api.smartproxy.com/v2/scrape', [
+$response = $client->request('POST', 'https://scraper-api.decodo.com/v2/scrape', [
   'body' => '{"target":"universal","parse":false,"url":"https://www.facebook.com/groups/1394454774138066"}',
   'headers' => [
     'Accept' => 'application/json',
